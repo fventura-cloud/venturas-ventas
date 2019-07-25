29bdcab330ee47b1dcb89de46bd6c9c69929e6f5
Fidel A. Ventura <falbertoventura@yahoo.com>
2019-07-24 21:54:01 -0600

    CRUD

--- src/routes/prods.js
+++ src/routes/prods.js
@@ -27,12 +27,32 @@ router.get('/prods/add', function(req, res) {  //url inventada
       // res.send("ok"); //En vez de este. Guardare los datos en la BD.
       const newProd = new Prod({title, description}); //newProd es objeto producto pero no en BD
       await newProd.save();  // *** 1:22:59 ****
-      res.send("OK")
+      res.redirect('/prods');
     }
+      });
+
+router.get('/prods', async function(req, res) {  //url inventada
+      const productos = await Prod.find().sort({date: 'desc'});
+      //console.log({productos})   **funciona**
+      res.render('prods/all-prods', { productos });
+      //res.send("Probando... Probando");
+   });
+
+   router.get('/prods/edit/:id', async function(req,res){
+     const prodEdit= await Prod.findById(req.params.id);
+     res.render('prods/edit-prod', {prodEdit});
+   })
+
+   router.put('/prods/edit-prod/:id', async function(req,res){
+    const {title, description} = req.body;
+    await Prod.findByIdAndUpdate(req.params.id, {title, description});
+    res.redirect('/prods');
   });
 
-  router.get('/prods', function(req, res) {  //url inventada
-    res.render("prods/new-prod");
+  router.delete('/prods/delete/:id', async function(req,res){
+    await Prod.findByIdAndDelete(req.params.id);
+    //console.log(req.params.id)
+    res.send("OK")
   });
 
 module.exports = router;
